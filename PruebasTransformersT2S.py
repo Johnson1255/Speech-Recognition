c97ad3ea5d5c684c2e20eaa1d5332aabd75a6a16
Johnson1255 <johnson.chen@upb.edu.co>
2024-05-27 00:28:04 -0500

    Pruebas de 2 Modelos de Text-To-Speech
    La descarga del segundo modelo, es muchisimo mas pesado, llevo mas de 1 hora esperando la descarga de dicho modelo, pero los componentes y cosas que tienes incorporadas son muy buenas

---
+++ PruebasTransformersT2S.py
@@ -0,0 +1,51 @@
+"""
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech
+
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+
+inputs = processor(text="HOLA COMO HAS ESTADO?", return_tensors="pt") #La voz le sale muy extrajero
+
+from datasets import load_dataset
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+
+import torch
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+spectrogram = model.generate_speech(inputs["input_ids"], speaker_embeddings)
+
+from transformers import SpeechT5HifiGan
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+with torch.no_grad():
+    speech = vocoder(spectrogram)
+
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+import soundfile as sf
+sf.write("tts_example.wav", speech.numpy(), samplerate=16000)
+"""
+
+from transformers import pipeline
+import scipy
+
+synthesiser = pipeline("text-to-speech", "suno/bark")
+
+speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
+
+scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
+
+from transformers import AutoProcessor, AutoModel
+
+processor = AutoProcessor.from_pretrained("suno/bark")
+model = AutoModel.from_pretrained("suno/bark")
+
+inputs = processor(
+    text=["Hello, my name is Suno. And, uh — and I like pizza. [laughs] But I also have other interests such as playing tic tac toe."],
+    return_tensors="pt",
+)
+
+speech_values = model.generate(**inputs, do_sample=True)
+
+sampling_rate = model.config.sample_rate
+scipy.io.wavfile.write("bark_out.wav", rate=sampling_rate, data=speech_values.cpu().numpy().squeeze())
